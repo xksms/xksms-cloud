daa3247bab18aa370725a053b1ad3865bfc1c208
xksms <1262381951@qq.com>
2025-06-25 16:09:55 +0800

    feat(infrastructure): 添加 ELK 日志收集分析系统
    - 新增 Docker Compose 配置文件，用于部署 Elasticsearch、Kibana 和 Logstash
    - 创建通用常量、枚举和工具类包
    - 添加日志规范化 starter- 构建项目基础结构和依赖管理

---
+++ xksms-common/src/main/java/cn/xksms/common/enums/YesNoEnum.java
@@ -0,0 +1,25 @@
+package cn.xksms.common.enums;
+
+import cn.xksms.common.core.IntArrayValuable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Arrays;
+
+@Getter
+@AllArgsConstructor
+public enum YesNoEnum implements IntArrayValuable {
+	YES(1, "是"),
+	NO(0, "否");
+
+	private final int code;
+	private final String desc;
+
+	public static final int[] ARRAYS = Arrays.stream(values()).mapToInt(YesNoEnum::getCode).toArray();
+
+
+	@Override
+	public int[] array() {
+		return ARRAYS;
+	}
+}
---
+++ xksms-common/src/main/java/cn/xksms/common/util/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author 程超
+ * @since 2025/6/25
+ * 包说明：工具类（DateUtils、JsonUtils、IdGenerator 等）
+ */
+package cn.xksms.common.util;
