39658afb1d9d42fa1b3a7692aaf7d5ae0ff05e0a
xksms <1262381951@qq.com>
2025-07-19 12:46:47 +0800

    feat(observability): 添加 Prometheus 和 Micrometer 集成
    - 实现了全局标签注册定制器，用于在 MeterRegistry 中添加通用标签
    - 添加了默认的 observability 配置文件，包含 Prometheus 和 Actuator 配置
    - 创建了自动配置类，用于自动配置 MeterRegistryCustomizer
    -定义了 ObservabilityProperties 类，用于配置全局标签
    - 更新了项目 pom.xml，添加了必要的依赖
    - 新增了 README.md，详细介绍了组件功能和使用方法

---
+++ xksms-starters/xksms-starter-observability/src/main/java/com/xksms/observability/config/ObservabilityPrometheusAutoConfig.java
@@ -0,0 +1,22 @@
+package com.xksms.observability.config;
+
+
+import com.xksms.observability.customizer.GlobalTagRegistryCustomizer;
+import com.xksms.observability.properties.ObservabilityProperties;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+@AutoConfiguration
+@EnableConfigurationProperties(ObservabilityProperties.class)
+public class ObservabilityPrometheusAutoConfig {
+
+	@Bean
+	@ConditionalOnMissingBean
+	public MeterRegistryCustomizer<MeterRegistry> metricsCommonTagsCustomizer(ObservabilityProperties properties) {
+		return new GlobalTagRegistryCustomizer(properties);
+	}
+}
