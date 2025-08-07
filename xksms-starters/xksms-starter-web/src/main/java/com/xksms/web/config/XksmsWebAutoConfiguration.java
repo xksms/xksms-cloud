ba585759dd68e5dbcb9d9880f8959c43f8a4922e
xksms <1262381951@qq.com>
2025-08-07 15:44:37 +0800

    refactor(xksms): 移除 WebFlux 相关代码并调整日志记录方式
    - 删除了 WebFlux 相关的观测和过滤代码
    -移除了自定义的日志切面
    - 调整了日志记录依赖，使用 Spring Boot 默认的日志记录机制
    - 清理了不必要的依赖项

--- xksms-starters/xksms-starter-web/src/main/java/com/xksms/web/config/XksmsWebAutoConfiguration.java
+++ xksms-starters/xksms-starter-web/src/main/java/com/xksms/web/config/XksmsWebAutoConfiguration.java
@@ -1,24 +1,11 @@
 package com.xksms.web.config;
 
 
-import com.xksms.log.properties.LogstashProperties;
-import com.xksms.web.aspect.WebLogAspect;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 
 @AutoConfiguration
-@EnableConfigurationProperties(LogstashProperties.class)
 public class XksmsWebAutoConfiguration {
 
-
-	@Bean
-	// 当业务项目引入了 aop starter 时，这个 Bean 才会生效
-	@ConditionalOnClass(name = "org.aspectj.lang.annotation.Aspect")
-	public WebLogAspect webLogAspect() {
-		return new WebLogAspect();
-	}
 
 
 }
